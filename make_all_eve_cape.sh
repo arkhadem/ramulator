802efee8c706de0649e851db9715fadcf300f202
Alireza Khadem <arkhadem@umich.edu>
2023-07-04 16:08:11 -0700

    make all updated

--- make_all_eve_cape.sh
+++ make_all_eve_cape.sh
@@ -1,4 +1,8 @@
 make clean
+make -j7 CFLAGS='-DEXE_TYPE=DVI_EXE -DISA_TYPE=LIME_ISA -DLANES_PER_CB=1024 -DLANES_PER_SA=256'
+mv ramulator ramulator_gpic
+
+make clean
 make -j7 CFLAGS='-DEXE_TYPE=INORDER_EXE -DISA_TYPE=LIME_ISA -DLANES_PER_CB=1024 -DLANES_PER_SA=256'
 mv ramulator ramulator_eve
 
@@ -9,6 +13,10 @@ mv ramulator ramulator_eve4
 make clean
 make -j7 CFLAGS='-DEXE_TYPE=INORDER_EXE -DISA_TYPE=LIME_ISA -DLANES_PER_CB=2048 -DLANES_PER_SA=64 -DLATENCY_FILE_NAME=\"cape_intrinsics_latency\"'
 mv ramulator ramulator_cape
+
+make clean
+make -j7 CFLAGS='-DDEBUG -DEXE_TYPE=DVI_EXE -DISA_TYPE=LIME_ISA -DLANES_PER_CB=1024 -DLANES_PER_SA=256'
+mv ramulator ramulator_gpic_debug
 
 make clean
 make -j7 CFLAGS='-DDEBUG -DEXE_TYPE=INORDER_EXE -DISA_TYPE=LIME_ISA -DLANES_PER_CB=1024 -DLANES_PER_SA=256'
